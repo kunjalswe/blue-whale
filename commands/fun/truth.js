60949271a430eaf3e8fcf0d450b24e51ca114948
Kunjal Joshi <kunjaldjoshi@gmail.com>
2026-04-27 17:30:58 +0545

    Update truth.js

--- commands/fun/truth.js
+++ commands/fun/truth.js
@@ -10,7 +10,7 @@ module.exports = {
       const res = await fetch("https://api.truthordarebot.xyz/v1/truth");
       const data = await res.json();
       const embed = new EmbedBuilder()
-        .setTitle("Truth")
+        .setTitle("🤔Truth")
         .setDescription(data.question)
         .setColor(0x2ECC71)
         .setFooter({ text: "Be honest 👀" })
